a5b1211ff8be9635b4a0495cc92138890a87b5d2
Stephanie Vilayphiou <stephanie@stdin.fr>
2010-04-14 20:11:48 +0200

    add user options to the py script

--- scripts/twitter.py
+++ scripts/twitter.py
@@ -15,14 +15,32 @@
 # You should have received a copy of the GNU General Public License
 # along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
-
+from optparse import OptionParser
 import urllib
 from BeautifulSoup import BeautifulSoup, BeautifulStoneSoup
 
+# Permet à l'utilisateur d'entrer une option avant d'exécuter le script
+parser = OptionParser()
+parser.add_option("-r", "--requete", dest="requete", default="", help="recherche le(s) mot(s) sur Twitter")
+parser.add_option("-l", "--langue", dest="langue", default="all", help="choix de la langue (code ISO)")
+parser.add_option("-t", "--tag", dest="tag", default="", help="chercher par un tag/theme precis")
+parser.add_option("-n", "--nombre", dest="nombre", default="10", help="nombre de resultats souhaite")
+parser.add_option("-d", "--debut", dest="debut", default="", help="recherche depuis une date precise, format aaaa-mm-jj")
+parser.add_option("-f", "--fin", dest="fin", default="", help="recherche jusqu'une date precise, format aaaa-mm-jj")
+(options, args) = parser.parse_args()
 
-requete = urllib.urlopen("http://search.twitter.com/search?ands=&from=&lang=fr&nots=&ors=&phrase=&q=&ref=&rpp=15&since=&tag=design&to=&units=km&until=&within=50")
-html = requete.read()
-requete.close()
+# récupère dans la variable "mots" les mots entrés par l'utilisateur
+requete = options.requete
+langue = options.langue
+tag = options.tag
+nombre = options.nombre
+debut = options.debut
+fin = options.fin
+
+url = "http://search.twitter.com/search?&tag=%s&ands=%s&nots=&ors=&phrase=&q=&ref=&rpp=%s&since=%s&until=%s&near=&within=15&units=km&from=&to=&lang=%s" % (tag, requete, nombre, debut, fin, langue)
+recherche = urllib.urlopen(url)
+html = recherche.read()
+recherche.close()
 
 soup = BeautifulSoup(html)
 resultats = soup('li', 'result')
@@ -38,5 +56,8 @@ for resultat in resultats:
         infos.span.extract()
     tweets += texte.text.encode('utf-8') + ' ' + infos.text.replace('&middot;', '').encode('utf-8') + '\n'
 
-tweets = BeautifulStoneSoup(tweets, convertEntities="html", smartQuotesTo="html").contents[0].encode('utf-8')
-print tweets.replace('&apos;', "'")
+try:
+    tweets = BeautifulStoneSoup(tweets, convertEntities="html", smartQuotesTo="html").contents[0].encode('utf-8')
+    print tweets.replace('&apos;', "'")
+except:
+    print "Il n'y a aucun résultat."
