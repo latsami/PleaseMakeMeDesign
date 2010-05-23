663065bc1321331f22e91452b929543a16e847c5
Stephanie Vilayphiou <stephanie@stdin.fr>
2010-05-23 15:27:48 +0200

    automatic fontsize to fit page vertically. rm header

--- scripts/layout.sh
+++ scripts/layout.sh
@@ -1,8 +1,9 @@
 #! /bin/bash -x
 
 #FONTSIZE=$((`wc -l | tr -d ' '`*6))
-#FONTSIZE=60
+#FONTSIZE=12
+PAGE=793
 MYLEN=$( cat $1 | wc -l | tr -d ' ')
-FONTSIZE=$((566/$MYLEN))
+FONTSIZE=$((($PAGE-($PAGE/$MYLEN))/$MYLEN))
 #enscript -f 'Helvetica-Bold@'$FONTSIZE'/'$FONTSIZE -p 'bla.ps' --non-printable-format=questionmark -T 1 --mark-wrapped-lines=arrow
-enscript -f 'Helvetica-Bold@'$FONTSIZE'/'$FONTSIZE -p 'bla.ps' --non-printable-format=questionmark -T 1 --mark-wrapped-lines=arrow $1
+enscript -f 'Helvetica-Bold@'$FONTSIZE'/'$FONTSIZE -p 'bla.ps' --non-printable-format=questionmark -T 8 -B --mark-wrapped-lines=arrow $1
